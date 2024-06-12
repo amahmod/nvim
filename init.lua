1914784716b0f15551e3d6f9df9a2068626dda59
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2024-06-12 12:09:00 +0600

    plugins: add colorscheme

--- init.lua
+++ init.lua
@@ -2,3 +2,27 @@ vim.g.mapleader = ' '
 
 require 'keymaps'
 require 'options'
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
+if not vim.loop.fs_stat(lazypath) then
+    print 'Installing lazy.nvim plugin manager...'
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        '--branch=stable',
+        'https://github.com/folke/lazy.nvim.git',
+        lazypath,
+    }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup {
+    spec = {
+        { import = 'plugins' },
+    },
+    chnage_detection = {
+        notify = false,
+    },
+}
