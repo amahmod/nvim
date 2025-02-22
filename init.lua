6c4cbbee3814d41f45bc79a998ae32590c5a5a01
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2025-02-22 11:56:44 +0600

    add lazy plugins manager and ropsepine colorscheme

---
+++ init.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = ' '
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
+if not vim.loop.fs_stat(lazypath) then
+    print 'Installing lazy.nvim plugin manager...'
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        '--branch=stable',
+        'https://github.com/folke/lazy.nvim.git',
+        lazypath,
+    }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup {
+    spec = {
+        {
+            import = 'plugins',
+            cond = function()
+                return not vim.g.vscode
+            end,
+        },
+    },
+    change_detection = {
+        notify = false,
+    },
+}
